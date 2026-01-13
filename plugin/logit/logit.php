690878a1f563920742cdf5dd7bacbe23c3df0861
SandeepJakka <sandeepjakka2624@gmail.com>
2026-01-13 21:27:38 +0530

    added logit plugin skeleton

---
+++ plugin/logit/logit.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: LogIt
+ * Description: Core plugin for the LogIt dashboard.
+ * Version: 0.1.0
+ * Author: Sandeep Jakka
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
